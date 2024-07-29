c0491f58400acc27211b0bf15426fb686cb1893e
elmurodDev <elmurodorinboyev@gmail.com>
2024-07-29 17:10:47 +0500

    Connect to db and delete local db and logics

--- controllers/posterController.js
+++ controllers/posterController.js
@@ -6,17 +6,22 @@ const {
   deleteById,
 } = require("../db/poster");
 const { v4 } = require("uuid");
+const Poster = require("../models/posterModel");
 
 // @route   GET /posters
 // @desc    Get all posters
 // @access  public
 const getPostersPage = async (req, res) => {
-  const posters = await getAllPosters();
-  res.render("poster/posters", {
-    title: "Posters page",
-    posters,
-    url: process.env.URL,
-  });
+  try {
+    const posters = await Poster.find().lean();
+    res.render("poster/posters", {
+      title: "Posters page",
+      posters: posters.reverse(),
+      url: process.env.URL,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getAddPoster = (req, res) => {
@@ -27,51 +32,64 @@ const getAddPoster = (req, res) => {
 };
 
 const getOnePoster = async (req, res) => {
-  const poster = await getPosterById(req.params.id);
-  res.render("poster/one", {
-    title: poster.title,
-    poster,
-    url: process.env.URL,
-  });
+  try {
+    const poster = await Poster.findById(req.params.id).lean();
+    res.render("poster/one", {
+      title: poster.title,
+      poster,
+      url: process.env.URL,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getPosterEditPage = async (req, res) => {
-  const poster = await getPosterById(req.params.id);
-  res.render("poster/update", {
-    title: "Update page",
-    poster,
-    url: process.env.URL,
-  });
+  try {
+    const poster = await Poster.findById(req.params.id).lean();
+    res.render("poster/update", {
+      title: "Update page",
+      poster,
+      url: process.env.URL,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const updatePosterById = async (req, res) => {
-  const poster = {
-    title: req.body.title,
-    amount: req.body.amount,
-    region: req.body.region,
-    image: req.body.image,
-    description: req.body.description,
-  };
-  await editPosterById(req.params.id, poster);
-  res.redirect("/posters");
+  try {
+    const poster = {
+      title: req.body.title,
+      amount: req.body.amount,
+      region: req.body.region,
+      image: req.body.image,
+      description: req.body.description,
+    };
+    await Poster.findByIdAndUpdate(req.params.id, poster);
+    res.redirect("/posters");
+  } catch (error) {}
 };
 
 const addPoster = async (req, res) => {
-  const poster = {
-    id: v4(),
-    title: req.body.title,
-    amount: req.body.amount,
-    region: req.body.region,
-    image: req.body.image,
-    description: req.body.description,
-  };
-  await addNewPosterToDb(poster);
-  res.redirect("/");
+  try {
+    const poster = {
+      title: req.body.title,
+      amount: req.body.amount,
+      region: req.body.region,
+      image: req.body.image,
+      description: req.body.description,
+    };
+    Poster.create(poster);
+    res.redirect("/posters");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const deletePoster = async (req, res) => {
   try {
-    await deleteById(req.params.id);
+    await Poster.findByIdAndDelete(req.params.id);
     res.redirect("/posters");
   } catch (error) {
     console.log(error);
